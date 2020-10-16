06d31ea42cf6fb8d16f7975851c6d8ef9aba2dfc
Burima Evgenyi <cuksmail@gmail.com>
2020-10-16 15:23:07 +0500

    what season ready

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = getSeason;
+function getSeason(date) {
+  //const now = new Date();
+  if (!date){
+    return 'Unable to determine the time of year!';
+  }
+  //if (now.getFullYear !== date.getFullYear || Object.getPrototypeOf(date) !== Date.prototype ){
+  if ( Object.keys(date).length > 0 ){
+    throw new Error('Not a Date');
+  }
+  const seasons = [
+    'spring',
+    'summer',
+    'autumn',
+    'winter'
+  ];
+  const month = date.getMonth();
+  if(month > 1 && month < 5) return seasons[0];
+  if(month > 4 && month < 8) return seasons[1];
+  if(month > 7 && month < 11) return seasons[2];
+  if(month > 10 || month < 2) return seasons[3];
+}
