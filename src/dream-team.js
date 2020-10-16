1ac5b95691a7158b011108aeeef0e52cb5e03f79
Burima Evgenyi <cuksmail@gmail.com>
2020-10-16 08:50:59 +0500

    dreamTeam ready

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  return Array.isArray(members) ?
+          members.map( name => {
+            if (isString(name)){
+              name = name.trim();
+              const letter = name[0].toUpperCase();
+              if(letter.match(/[A-Z]/)) return letter;
+            }
+          }).sort().join('') :
+          false;
 };
+
+function isString(word){
+  return typeof word === typeof '';
+}
