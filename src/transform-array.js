f82a8873a03e60e23d731bb3f2c8963f5ca3b06c
Burima Evgenyi <cuksmail@gmail.com>
2020-10-17 13:36:47 +0500

    transform array completed

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
+module.exports = transform;
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('Not an Array');
+  const initArr = [...arr];
+  const result = [];
+  let isContinue = false;
+
+  for (let i = 0; i < initArr.length; i++){
+    if(isContinue){
+      isContinue = !isContinue;
+      continue;
+    }
+    const arrElement = initArr[i];
+    const nextEl = initArr[i + 1];
+    if(nextEl == '--discard-prev' || arrElement == '--discard-prev' || arrElement == '--double-prev'){
+      continue;
+    }
+    if(nextEl == '--double-prev'){
+      result.push(arrElement);
+      result.push(arrElement);
+      continue;
+    }
+    if(arrElement == '--discard-next'){
+      isContinue = true;
+      continue;
+    }
+    if(arrElement == '--double-next'){
+      if(nextEl !== undefined){
+        result.push(nextEl);
+      }
+      continue;
+    }
+    result.push(arrElement);
+  }
+
+  return result;
+}
